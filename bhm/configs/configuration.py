0866069b5a3e1a68b4f08de11dcb91d52de7b600
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2022-10-29 15:51:41 +0100

    config

--- bhm/configs/configuration.py
+++ bhm/configs/configuration.py
@@ -5,3 +5,19 @@ Code: The main configuration file for the project.
 Project: Inferring the tomographic redshift distribution of the KiDS-1000
 catalogue.
 """
+import ml_collections
+
+
+def get_config():
+
+    config = ml_collections.ConfigDict()
+
+    # bands
+    config.band = ['u', 'g', 'r', 'i', 'Z', 'Y', 'J', 'H', 'Ks']
+
+    # flux
+    config.flux = flux = ml_collections.ConfigDict()
+    config.flux.names = [f'FLUX_GAAP_{b}' for b in config.band]
+    config.flux.err_names = [f'FLUXERR_GAAP_{b}' for b in config.band]
+
+    return config
