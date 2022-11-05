a5e4c333b5ff977e79964f8649904cc3a3a1ea91
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2022-11-05 10:48:05 +0000

    configurations

---
+++ bhm/configuration.py
@@ -0,0 +1,39 @@
+"""
+Author: Arrykrishna Mootoovaloo
+Date: October 2022
+Code: The main configuration file for the project.
+Project: Inferring the tomographic redshift distribution of the KiDS-1000
+catalogue.
+"""
+import ml_collections
+
+
+def config():
+
+    config = ml_collections.ConfigDict()
+
+    # bands
+    config.band = ['u', 'g', 'r', 'i', 'Z', 'Y', 'J', 'H', 'Ks']
+
+    # flux
+    config.flux = flux = ml_collections.ConfigDict()
+    flux.value_cols = [f'FLUX_GAAP_{b}' for b in config.band]
+    flux.err_cols = [f'FLUXERR_GAAP_{b}' for b in config.band]
+
+    # magnitudes
+    config.mag = mag = ml_collections.ConfigDict()
+    mag.value_col = [f'MAG_GAAP_{b}' for b in band]
+    mag.err_col = [f'MAGERR_GAAP_{b}' for b in band]
+
+    # columns related to BPZ
+    config.bpz_cols = ['Z_B', 'M_0', 'Z_ML', 'Z_B_MIN', 'Z_B_MAX']
+
+    # other important columns
+    config.meta_cols = ['ALPHA_J2000', 'DELTA_J2000', 'e1', 'e2', 'weight']
+
+    # some quantities for size of arrays
+    config.nbpz = len(config.bpz_cols)
+    config.nmeta = len(config.meta_cols)
+    config.nband = len(config.band)
+
+    return config
--- bhm/src/processing.py
+++ bhm/src/processing.py
@@ -5,3 +5,31 @@ Code: Function to process the data in the right format.
 Project: Inferring the tomographic redshift distribution of the KiDS-1000
 catalogue.
 """
+
+import sys
+import pandas as pd
+
+from configuration import config
+
+CONFIG = config()
+
+
+def get_size_mb(dataframe: pd.DataFrame):
+    """Print the size of the pandas dataframe in MB
+
+    Args:
+        dataframe (pd.DataFrame): the pandas dataframe
+    """
+    print(f'Size of dataframe {sys.getsizeof(dataframe)/1024**2:.2f} MB')
+
+
+def split_data(cataloguengalaxies: int, save: bool) -> dict:
+    """Split the full data into sets of galaxies. These are randomly chosen from the full catalogue.
+
+    Args:
+        ngalaxies (int): _description_
+        save (bool): _description_
+
+    Returns:
+        dict: _description_
+    """
