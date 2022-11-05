63623c46a423e3f825cdeacfa59e6c15f50cf65d
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2022-11-05 11:35:31 +0000

    processing

--- bhm/configuration.py
+++ bhm/configuration.py
@@ -22,8 +22,8 @@ def config():
 
     # magnitudes
     config.mag = mag = ml_collections.ConfigDict()
-    mag.value_col = [f'MAG_GAAP_{b}' for b in config.band]
-    mag.err_col = [f'MAGERR_GAAP_{b}' for b in config.band]
+    mag.value_cols = [f'MAG_GAAP_{b}' for b in config.band]
+    mag.err_cols = [f'MAGERR_GAAP_{b}' for b in config.band]
 
     # columns related to BPZ
     config.bpz_cols = ['Z_B', 'M_0', 'Z_ML', 'Z_B_MIN', 'Z_B_MAX']
