e8f33249ee4cf3ffde62945b4a8671aa4d1b7b5b
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2023-05-26 15:42:01 +0100

    notebook

--- reference/BL-2016/bayeshist.py
+++ reference/BL-2016/bayeshist.py
@@ -5,7 +5,7 @@ import sys
 import time
 
 # 	Input 1 (required) : file name for inputs (likelihoods)
-# 	Input 2 (required) : number of Gibbs samples 
+# 	Input 2 (required) : number of Gibbs samples
 # 	Input 3 (optional) : file name for input initialisation
 
 # Parse inputs
@@ -21,29 +21,31 @@ MPI_size = comm.Get_size()
 MPI_rank = comm.Get_rank()
 
 if MPI_rank == 0:
-    print('Input parameters:', sys.argv)
-    print('Running on %d cores' % MPI_size)
+    print("Input parameters:", sys.argv)
+    print("Running on %d cores" % MPI_size)
+
 
 def dirichlet(rsize, alphas):
-    gammabs = np.array([np.random.gamma(alpha+1, size=rsize) for alpha in alphas])
+    gammabs = np.array([np.random.gamma(alpha + 1, size=rsize) for alpha in alphas])
     fbs = gammabs / gammabs.sum(axis=0)
     return fbs.T
 
-pdfints_npfile = infilename+'_'+str(MPI_rank+1)+'.npy'
+
+pdfints_npfile = infilename + "_" + str(MPI_rank + 1) + ".npy"
 pdfints = np.load(pdfints_npfile)
 sh = pdfints.shape
 nobj = sh[0]
-print('Read file', pdfints_npfile, 'and found', nobj, 'objects')
+print("Read file", pdfints_npfile, "and found", nobj, "objects")
 nbins = np.prod(sh[1:])
 pdfints = pdfints.reshape((nobj, nbins))
 
 
 if MPI_rank == 0:
     if len(sys.argv) > 3:
-        print('Initialised sampler with file', fname_ini)
+        print("Initialised sampler with file", fname_ini)
         hbs = np.load(fname_ini).reshape((nbins,))
         hbs /= hbs.sum()
-    else:# random initialisation
+    else:  # random initialisation
         nbs = np.random.rand(nbins)
         hbs = dirichlet(1, nbs)
 else:
@@ -53,23 +55,24 @@ comm.Barrier()
 
 hbs = comm.bcast(hbs, root=0)
 if MPI_rank == 0:
-    print('Broadcasted hbs')
+    print("Broadcasted hbs")
 
 if MPI_rank == 0:
-    fbs = np.zeros( (nsamples, nbins) )
+    fbs = np.zeros((nsamples, nbins))
     tstart = time.time()
 
 comm.Barrier()
-ibins = np.repeat(np.arange(1, nbins), nobj).reshape((nbins-1, nobj)).T.ravel()
+ibins = np.repeat(np.arange(1, nbins), nobj).reshape((nbins - 1, nobj)).T.ravel()
 for kk in range(1, nsamples):
-
-    prods = (pdfints * hbs)
-    cumsumweights = np.add.accumulate(prods, axis=1).T #cumsumweights = prods.cumsum(axis=1).T 
-    cumsumweights /= cumsumweights[-1,:]
+    prods = pdfints * hbs
+    cumsumweights = np.add.accumulate(
+        prods, axis=1
+    ).T  # cumsumweights = prods.cumsum(axis=1).T
+    cumsumweights /= cumsumweights[-1, :]
     pos = np.random.uniform(0.0, 1.0, size=nobj)
-    cond = np.logical_and(pos > cumsumweights[:-1,:], pos <= cumsumweights[1:,:])
+    cond = np.logical_and(pos > cumsumweights[:-1, :], pos <= cumsumweights[1:, :])
     res = np.zeros(nobj, dtype=int)
-    res[pos <= cumsumweights[0,:]] = 0
+    res[pos <= cumsumweights[0, :]] = 0
     locs = np.any(cond, axis=0)
     res[locs] = ibins[cond.T.ravel()]
     ind_inrange = np.logical_and(res > 0, res < nbins)
@@ -79,19 +82,25 @@ for kk in range(1, nsamples):
     comm.Allreduce(nbs, nbs_all, op=MPI.SUM)
 
     if MPI_rank == 0:
-
         if kk % 10 == 0:
-            #print kk
+            # print kk
             tend = time.time()
-            fname = infilename + '_post.npy'
-            ss = int(burnin_fraction*kk)
-            sh2 = tuple([kk-ss]+list(sh[1:]))
-            print('Saving', kk-ss, 'samples to', fname, '(%.2f' % (float(tend-tstart)/kk), 'sec per sample)')
+            fname = infilename + "_post.npy"
+            ss = int(burnin_fraction * kk)
+            sh2 = tuple([kk - ss] + list(sh[1:]))
+            print(
+                "Saving",
+                kk - ss,
+                "samples to",
+                fname,
+                "(%.2f" % (float(tend - tstart) / kk),
+                "sec per sample)",
+            )
             np.save(fname, fbs[ss:kk, :].reshape(sh2))
 
-        hbs = dirichlet(1, nbs_all) #### PLUS ONE HERE OR NOT?? ???? ??? 
+        hbs = dirichlet(1, nbs_all)  #### PLUS ONE HERE OR NOT?? ???? ???
 
     hbs = comm.bcast(hbs, root=0)
 
     if MPI_rank == 0:
-        fbs[kk,:] = hbs
+        fbs[kk, :] = hbs
